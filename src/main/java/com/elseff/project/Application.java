f8db1365b32681f3fc353449903bbef1669a0b07
Danila <danilapetrov522@gmail.com>
2022-06-22 15:29:05 +0400

    Task-1: Transfer bean ModelMapper to config

--- src/main/java/com/elseff/project/Application.java
+++ src/main/java/com/elseff/project/Application.java
@@ -1,19 +1,12 @@
 package com.elseff.project;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    ModelMapper getModelMapper() {
-        return new ModelMapper();
     }
 }
---
+++ src/main/java/com/elseff/project/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.elseff.project.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
