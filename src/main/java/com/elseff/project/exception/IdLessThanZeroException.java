b01bbd0f9c8a53fb4c0bf1e462e6473b4525c9d9
Danila <danilapetrov522@gmail.com>
2022-06-25 11:47:33 +0400

    Task-3: formatted code

--- src/main/java/com/elseff/project/exception/IdLessThanZeroException.java
+++ src/main/java/com/elseff/project/exception/IdLessThanZeroException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class IdLessThanZeroException extends RuntimeException{
-    public IdLessThanZeroException(){
+public class IdLessThanZeroException extends RuntimeException {
+    public IdLessThanZeroException() {
         super("id must be a greater than 0");
     }
 }
