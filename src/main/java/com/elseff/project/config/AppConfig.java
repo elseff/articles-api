e6bb3e9967f40b24970f025987e900bfc55297b4
Danila <danilapetrov522@gmail.com>
2022-06-25 11:47:15 +0400

    Task-3: formatted code

--- src/main/java/com/elseff/project/config/AppConfig.java
+++ src/main/java/com/elseff/project/config/AppConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
     @Bean
-    ModelMapper modelMapper(){
+    ModelMapper modelMapper() {
         return new ModelMapper();
     }
 }
